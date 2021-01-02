587705d1057dfbf2786108aca9df8fbee0cc10a9
Space_Fox <spacefox@FoxStarship>
2021-01-02 16:12:48 +0300

    Fixed 3rd lecture. 5th paragraph ended. 1st lecture checked and corrected.

---
+++ Lectures/5_Semester/Theory_of_Functions_of_a_Complex_Variable/auto/main.el
@@ -0,0 +1,40 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "etc/preamble"
+    "etc/title_page"
+    "lectures/lect01"
+    "lectures/lect02"
+    "lectures/lect03"
+    "lectures/lect04"
+    "lectures/lect05"
+    "lectures/lect06"
+    "lectures/lect07"
+    "lectures/lect08"
+    "lectures/lect09"
+    "lectures/lect10"
+    "lectures/lect11"
+    "lectures/lect12"
+    "lectures/lect13"
+    "lectures/lect14"
+    "lectures/lect15"
+    "lectures/lect16"
+    "lectures/lect17"
+    "lectures/lect18"
+    "lectures/lect19"
+    "lectures/lect20"
+    "lectures/lect21"
+    "lectures/lect22"
+    "lectures/lect23"
+    "etc/additional"
+    "article"
+    "art12")
+   (TeX-add-symbols
+    "CourseName"
+    "CourseDate"))
+ :latex)
+
