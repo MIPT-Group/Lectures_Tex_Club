3b98dce6e54891f6fc7942873f4cb4e106e9c366
Space_Fox <spacefox@FoxStarship>
2021-01-04 00:47:11 +0300

    Ended 20 paragraph.

---
+++ Lectures/5_Semester/Theory_of_Functions_of_a_Complex_Variable/lectures/auto/lect15.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "lect15"
+ (lambda ()
+   (LaTeX-add-labels
+    "(19.2)"
+    "(19.3)"
+    "(19.4)"
+    "(19.5)"
+    "(19.6)"
+    "(19.7)"
+    "(19.8)"
+    "fig:20.1"))
+ :latex)
+
