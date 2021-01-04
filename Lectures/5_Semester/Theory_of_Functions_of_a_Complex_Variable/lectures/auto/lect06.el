d746ab80258894c63c814f32e4a3d3eea45ccf31
Space_Fox <spacefox@FoxStarship>
2021-01-05 00:24:17 +0300

    Ended 21 paragraph. Ended 16 lecture. Fixed 1 to 7 paragraphs.

---
+++ Lectures/5_Semester/Theory_of_Functions_of_a_Complex_Variable/lectures/auto/lect06.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "lect06"
+ (lambda ()
+   (LaTeX-add-labels
+    "(8.1)"
+    "(8.2)"
+    "(8.3)"
+    "(8.4)"
+    "(9.1)"
+    "(9.2)"
+    "(9.3)"
+    "(9.4)"))
+ :latex)
+
