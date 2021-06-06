36952ebe436ae9943d3fe3ab34d458bbd50b3657
Space_Fox <spacefox@FoxStarship>
2021-06-06 14:35:40 +0300

    added lectures to 9.

---
+++ Lectures/6_Semester/Random_Processes/2021_Shirobokov/auto/main.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("etc/note" "12pt" "twoside" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "etc/preamble"
+    "etc/letters"
+    "units/01"
+    "units/02"
+    "units/03"
+    "units/04"
+    "units/05"
+    "units/06"
+    "units/07"
+    "units/08"
+    "units/09"
+    "etc/note"
+    "etc/note12")
+   (TeX-add-symbols
+    "CourseDate"
+    "CourseName"))
+ :latex)
+
